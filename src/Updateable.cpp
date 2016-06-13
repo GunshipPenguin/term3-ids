01ab650b01529001867f173bc7c158c55336afbb
GunshipPenguin <GunshipPenguin@users.noreply.github.com>
2016-06-12 22:27:45 -0700

    Actually calculate time delta in Updatable::updateDelta()

--- src/Updateable.cpp
+++ src/Updateable.cpp
@@ -6,8 +6,7 @@ double Updateable::delta_;
 double Updateable::totalTime_;
 
 void Updateable::updateDelta() {
-	delta_ = 0.1;
-	/*double tempTime(clock_.getElapsedTime().asSeconds());
+	double tempTime(clock_.getElapsedTime().asSeconds());
 	delta_ = tempTime-totalTime_;
-	totalTime_=tempTime;*/
+	totalTime_=tempTime;
 }
